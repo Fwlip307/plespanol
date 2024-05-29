62fdb69c1af6f2f0ad5a4f3cd5c923c1e0a05b87
unknown <Laboratorio@MPAPCA03LC0118.duoc.acad>
2024-05-29 13:51:31 -0400

    commit inicial

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    imagen = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nombre
